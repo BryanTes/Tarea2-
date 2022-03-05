0439e6c3343c3540d1335529d0fc8ee2a114667a
BryanTes <72046247+BryanTes@users.noreply.github.com>
2022-03-04 21:55:48 -0600

    Tarea2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Esto va en el archivo program.cs
+
+using TAREA2;
+
+Console.WriteLine("Primera parte");
+Console.WriteLine("-------------");
+
+fecha date1 = new fecha(2022, 3, 4);
+Console.WriteLine(date1);
+
+
